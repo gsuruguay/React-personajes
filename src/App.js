ec9f1877443f8c983c61b92398975d5ffa3ac5f9
gsuruguay <guillermo.suruguay@gmail.com>
2021-10-01 16:32:23 -0300

    Modification of App to functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "./Components/NavBar/Navbar";
 import Home from "./Components/Home/Home";
 import personajes from "./personajes.json";
 import CharacterDetail from "./Components/CharacterDetail/CharacterDetail";
 import About from "./Components/About/About";
 
-class App extends React.Component {
+export default function App() {
+  const [characters, setCharacters]= useState(personajes.Characters);
+  
+
+  return (
+      <div>
+        <Navbar />
+        <Home characters={characters}/>
+        <CharacterDetail />
+        <About />
+      </div>
+    );
+}
+
+
+
+/* class App extends React.Component {
   state={
     characters: personajes.Characters
   }
@@ -22,4 +38,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default App; */
