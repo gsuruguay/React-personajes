105684dd9e47f39f732287e5bb0b33962afcdb44
gsuruguay <guillermo.suruguay@gmail.com>
2021-10-01 22:50:40 -0300

    About Component is modified

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import "./About.css";
 
-class About extends React.Component {
+export default function About() {
 
-    render() {
         return (
             <div className="divAbout">
                 <div className="row">
@@ -12,20 +11,32 @@ class About extends React.Component {
                     </div>
                     <div className="cont-Dev">
                         <h1>Guillermo Germán Suruguay</h1>
+                        <h3>Desarrollador Web FullStack <a href="https://www.linkedin.com/in/guillermo-suruguay-desarrollador-web" target="_blank" rel="noreferrer">
+                            <img className="ico-In" src="https://i.postimg.cc/HWSJWykH/icono-linkedin.png" alt="icono linkedin" /></a></h3>
+                        
                         <p>Soy una persona con más de +5 años de experiencia liderando equipos de trabajo y +5 años de experiencia en atención al cliente y ventas, con enfoque siempre en el logro de objetivos. Busco insertarme en una empresa del sector IT como Programador Web Jr. donde pueda desarrollarme y aportar mis conocimientos, aptitudes comunicativas y de trabajo en equipo. Soy autodidacta, con fuerte capacidad de aprendizaje, entusiasmo y planificación.
                         </p>
+                            <h3>Skills</h3>
                         <div className="icono">
-                            <a href="https://www.linkedin.com/in/guillermo-suruguay-desarrollador-web" target="_blank" rel="noreferrer">
-                            <img src="https://i.postimg.cc/HWSJWykH/icono-linkedin.png" alt="icono linkedin" /></a>
+                        <img src="https://i.postimg.cc/qv4kV3mc/javascript-icon-130900.png" alt="icono" />
+                        <img src="https://i.postimg.cc/wxRxhHyN/nodejs-original-wordmark-logo-icon-146412-1.png" alt="icono" />
+                        <img src="https://i.postimg.cc/fTxwrKvT/php-icon-130857.png" alt="icono" />
+                        <img src="https://i.postimg.cc/Gh0LKNG5/html-original-wordmark-logo-icon-146478.png" alt="icono" />
+                        <img src="https://i.postimg.cc/PJDthzsy/file-type-css-icon-130661.png" alt="icono" />
+                        <img src="https://i.postimg.cc/RFyMjp8q/mysql-original-wordmark-logo-icon-146417.png" alt="icono" />
+                        <img src="https://i.postimg.cc/T30TR3Sz/mongo-DB-logo-icon-146412.png" alt="icono" />
+                        <img src="https://i.postimg.cc/85jpXgZq/react-original-logo-icon-146374.png" alt="icono" />
+                        <img src="https://i.postimg.cc/PqfrZwYf/bootstrap-plain-logo-icon-146619.png" alt="icono" />
+                        <img src="https://i.postimg.cc/R0PCTZL7/git-original-wordmark-logo-icon-146510.png" alt="icono" />
+                        <img src="https://i.postimg.cc/FHVH7Zfn/1485282157-adobe-photoshop-raster-graphics-editor-cc-creative-cloud-78285.png" alt="icono" />
                         </div>
                     </div>
                 </div>
                 <footer>
-                    <a href="https://www.linkedin.com/in/guillermo-suruguay-desarrollador-web" target="_blank" rel="noreferrer">Dev: Guilermo Suruguay</a>
+                    <a href="https://www.linkedin.com/in/guillermo-suruguay-desarrollador-web" target="_blank" rel="noreferrer">Dev: Guilermo Suruguay </a>
                 </footer>
             </div>
         )
     }
-}
 
-export default About;
+
